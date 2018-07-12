1205880d9b9af009d9373c08c69ef48efc1ee1a2
diok22 <Dionysis.k22@gmail.com>
2018-07-12 09:57:42 +0100

    print one asterisk on console

---
+++ source/TriangleExercises.java
@@ -0,0 +1,17 @@
+class TriangleExercises {
+
+  void easiestExerciseEver() {
+    System.out.println("*");
+  }
+
+}
+
+class TriangleExercisesTestDrive {
+  public static void main(String[] args) {
+
+    TriangleExercises triangle = new TriangleExercises();
+
+    triangle.easiestExerciseEver();
+  }
+
+}
