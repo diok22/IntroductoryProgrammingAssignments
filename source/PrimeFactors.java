f6e813b1457e1d105de3ffdd6871abcad604ac0c
diok22 <Dionysis.k22@gmail.com>
2018-07-12 14:56:34 +0100

    removes empty lines

--- source/PrimeFactors.java
+++ source/PrimeFactors.java
@@ -4,42 +4,32 @@ import java.util.*;
 import java.io.*;
 
 class PrimeFactors {
-
   void generate(int n) {
     ArrayList<Integer> factors = new ArrayList<Integer>();
-
     int incrementer = n % 2 == 0 ? 1 : 2;
-    for (int i = 1; i <= Math.sqrt(n); i += incrementer) {
-
+    for (int i = 1; i <= Math.sqrt(n); i += 1) {
         if (n % i == 0 ) {
             factors.add(i);
-
             if (i != n / i) {
                 factors.add(n / i);
             }
         }
     }
 
-
     Collections.sort(factors);
-
     System.out.println(factors);
   }
 
   public static void main(String[] args) {
     PrimeFactors prime = new PrimeFactors();
-
     prime.generate(30);
   }
 
-
 }
-
 
 class PrimeFactorsTestDrive {
   public static void main(String[] args) {
     PrimeFactors prime = new PrimeFactors();
-
     prime.generate(30);
   }
 
