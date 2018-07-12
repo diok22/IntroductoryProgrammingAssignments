799238d03a357124f6435f632e037315ee3749ed
diok22 <Dionysis.k22@gmail.com>
2018-07-12 10:05:56 +0100

    given n print n asterisks

--- source/TriangleExercises.java
+++ source/TriangleExercises.java
@@ -4,6 +4,14 @@ class TriangleExercises {
     System.out.println("*");
   }
 
+  void drawAHorizonalLine(int n) {
+    String asterisk = "";
+    for (int i = 0; i < n; i++) {
+      asterisk += "*";
+    }
+    System.out.println(asterisk);
+  }
+
 }
 
 class TriangleExercisesTestDrive {
@@ -11,7 +19,10 @@ class TriangleExercisesTestDrive {
 
     TriangleExercises triangle = new TriangleExercises();
 
+    System.out.println("Print one asterisk to the console");
     triangle.easiestExerciseEver();
+    System.out.println("Given Given a number n, print n asterisks on one line.");
+    triangle.drawAHorizonalLine(8);
   }
 
 }
