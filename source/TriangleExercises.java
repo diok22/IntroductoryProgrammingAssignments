dc3a6b067dfa6a2f74557b36be0cd3e04c0608d0
diok22 <Dionysis.k22@gmail.com>
2018-07-12 10:22:55 +0100

    print right angle triangle

--- source/TriangleExercises.java
+++ source/TriangleExercises.java
@@ -5,22 +5,34 @@ class TriangleExercises {
   }
 
   void drawAHorizonalLine(int n) {
-    String asterisk = "";
+    String asterisks = "";
     for (int i = 0; i < n; i++) {
-      asterisk += "*";
+      asterisks += "*";
     }
-    System.out.println(asterisk);
+    System.out.println(asterisks);
   }
 
   void drawAVerticalLine(int n) {
-    String asterisk = "";
+    String asterisks = "";
     for (int i = 0; i < n; i++) {
-      asterisk += ("*\n");
+      asterisks += ("*\n");
     }
-    System.out.println(asterisk);
+    System.out.println(asterisks);
   }
 
+  void drawARightAngle(int n) {
+    String asterisks = "";
+    for (int x = 0; x < n; x++) {
+      for (int i = 0; i <= x; i++)
+          asterisks +=("*");
+          asterisks +=("\n");
+    }
+    System.out.println(asterisks);
+  }
+
+
 }
+
 
 class TriangleExercisesTestDrive {
   public static void main(String[] args) {
@@ -33,6 +45,8 @@ class TriangleExercisesTestDrive {
     triangle.drawAHorizonalLine(8);
     System.out.println("Given a number n, print n lines, each with one asterisks");
     triangle.drawAVerticalLine(3);
+    System.out.println("Given a number n, print n lines, each with one more asterisk than the last");
+    triangle.drawARightAngle(3);
   }
 
 }
