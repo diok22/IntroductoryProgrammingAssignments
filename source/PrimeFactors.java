63f8addad1ac9ce74aadd7bf1d2b15b78d274575
diok22 <Dionysis.k22@gmail.com>
2018-07-12 12:02:11 +0100

    prime numbers adds odd numbers too

---
+++ source/PrimeFactors.java
@@ -0,0 +1,46 @@
+import java.lang.Math;
+import java.lang.Comparable;
+import java.util.*;
+import java.io.*;
+
+class PrimeFactors {
+
+  void generate(int n) {
+    ArrayList<Integer> factors = new ArrayList<Integer>();
+
+    int incrementer = n % 2 == 0 ? 1 : 2;
+    for (int i = 1; i <= Math.sqrt(n); i += incrementer) {
+
+        if (n % i == 0) {
+            factors.add(i);
+
+            if (i != n / i) {
+                factors.add(n / i);
+            }
+        }
+    }
+    
+
+    Collections.sort(factors);
+
+    System.out.println(factors);
+  }
+
+  public static void main(String[] args) {
+    PrimeFactors prime = new PrimeFactors();
+
+    prime.generate(30);
+  }
+
+
+}
+
+
+class PrimeFactorsTestDrive {
+  public static void main(String[] args) {
+    PrimeFactors prime = new PrimeFactors();
+
+    prime.generate(30);
+  }
+
+}
