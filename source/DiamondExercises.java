edb6678495f3ae2abd1614464700a93043f5b446
diok22 <Dionysis.k22@gmail.com>
2018-07-12 10:41:18 +0100

    isosceles triangle

--- source/DiamondExercises.java
+++ source/DiamondExercises.java
@@ -1,13 +1,27 @@
 class DiamondExercises {
-  
 
-
+  void isoscelesTriangle(int n) {
+    String asterisks = "";
+    for (int i = 1; i < n * 2; i += 2) {
+        for (int x = 1; x < (n - (i / 2)); x++) {
+            asterisks +=(" ");
+        }
+        for (int x = 0; x < i; x++) {
+          asterisks +=("*");
+        }
+        asterisks +=("\n");
+    }
+    System.out.println(asterisks);
+  }
 
 }
 
 class DiamondExercisesTestDrive {
   public static void main(String[] args) {
 
+    DiamondExercises diamond = new DiamondExercises();
 
+    System.out.println("Given a number n, print a centered triangle");
+    diamond.isoscelesTriangle(3);
   }
 }
