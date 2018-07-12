9d2f15cad6044efddae867c67a8a95402574a24e
diok22 <Dionysis.k22@gmail.com>
2018-07-12 10:12:17 +0100

    print vertical line

--- source/TriangleExercises.java
+++ source/TriangleExercises.java
@@ -12,6 +12,14 @@ class TriangleExercises {
     System.out.println(asterisk);
   }
 
+  void drawAVerticalLine(int n) {
+    String asterisk = "";
+    for (int i = 0; i < n; i++) {
+      asterisk += ("*\n");
+    }
+    System.out.println(asterisk);
+  }
+
 }
 
 class TriangleExercisesTestDrive {
@@ -23,6 +31,8 @@ class TriangleExercisesTestDrive {
     triangle.easiestExerciseEver();
     System.out.println("Given Given a number n, print n asterisks on one line.");
     triangle.drawAHorizonalLine(8);
+    System.out.println("Given a number n, print n lines, each with one asterisks");
+    triangle.drawAVerticalLine(3);
   }
 
 }
