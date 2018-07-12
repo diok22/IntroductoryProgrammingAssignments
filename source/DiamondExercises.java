265ff98a30adbde0512922de19a044ce80015f6a
diok22 <Dionysis.k22@gmail.com>
2018-07-12 10:52:58 +0100

    diamond needs fix

--- source/DiamondExercises.java
+++ source/DiamondExercises.java
@@ -14,6 +14,22 @@ class DiamondExercises {
     System.out.println(asterisks);
   }
 
+  void centeredDiamond(int n) {
+    isoscelesTriangle(n);
+    String downAsterisks = "";
+    for (int i = n * 2; i > 1; i -= 2) {
+      for (int x = (n - (i / 2) + 1); x > 0; x--) {
+        downAsterisks += (" ");
+      }
+      for (int x = i; x > 1; x--) {
+        downAsterisks += ("*");
+      }
+      downAsterisks += ("\n");
+    }
+    System.out.println(downAsterisks);
+  }
+
+
 }
 
 class DiamondExercisesTestDrive {
@@ -23,5 +39,7 @@ class DiamondExercisesTestDrive {
 
     System.out.println("Given a number n, print a centered triangle");
     diamond.isoscelesTriangle(3);
+    System.out.println("Given a number n, print a centered diamond");
+    diamond.centeredDiamond(3);
   }
 }
