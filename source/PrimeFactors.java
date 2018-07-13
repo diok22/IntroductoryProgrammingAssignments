d78df8b7515396c5fb041d33cef15d14647f87d6
diok22 <Dionysis.k22@gmail.com>
2018-07-13 10:29:10 +0100

    completes prime factors

--- source/PrimeFactors.java
+++ source/PrimeFactors.java
@@ -1,22 +1,14 @@
 import java.lang.Math;
-import java.lang.Comparable;
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
 
 class PrimeFactors {
   void generate(int n) {
     ArrayList<Integer> factors = new ArrayList<Integer>();
-    int incrementer = n % 2 == 0 ? 1 : 2;
-    for (int i = 1; i <= Math.sqrt(n); i += 1) {
+    for (int i = 2; i <= Math.sqrt(n); i += 1) {
         if (n % i == 0 ) {
             factors.add(i);
-            if (i != n / i) {
-                factors.add(n / i);
-            }
         }
     }
-
-    Collections.sort(factors);
     System.out.println(factors);
   }
 
@@ -25,14 +17,4 @@ class PrimeFactors {
     prime.generate(30);
   }
 
-
 }
-/**
-class PrimeFactorsTestDrive {
-  public static void main(String[] args) {
-    PrimeFactors prime = new PrimeFactors();
-    prime.generate(30);
-  }
-
-}
-*/
